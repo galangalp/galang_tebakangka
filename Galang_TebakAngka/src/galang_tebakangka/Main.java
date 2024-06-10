46cebc3d81a4ce2f11aa9a9cf9ad0ac5a8e07119
D-14 <D-14@D-14>
2024-06-10 20:34:17 +0700

    upload tugas galang

---
+++ Galang_TebakAngka/src/galang_tebakangka/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package galang_tebakangka;
+
+/**
+ *
+ * @author D-14
+ */
+public class Main {
+    public static void main(String[] args) {
+        Galang_TebakAngka game=  new Galang_TebakAngka(10);
+        game.play();
+    }
+}
